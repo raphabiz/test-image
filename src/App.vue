6ff5028284664776f5e6e6a407c9beba79342c6e
raphabiz <56385968+raphabiz@users.noreply.github.com>
2023-05-11 13:53:27 +0200

    test a

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-container>
+    <h1>
+      L'Appareil d'Ilizarov ( appareil de Forrest Gump )
+    </h1>
+    <img  style="width: 300px;margin:auto;"
+      src="https://ik.imagekit.io/thumbnail/Test/poihuyvtyd.webp?updatedAt=1683805914356" />
+    <h2>
+      Création de l'appareil d'Ilizarov
+    </h2>
+    <h3>
+      Dans les années 1950, Gavriil Abramovich Ilizarov travaillait comme chirurgien militaire dans l'armée soviétique. Il
+      a remarqué que les os des soldats blessés guérissaient plus rapidement lorsqu'ils étaient immobilisés dans des
+      fixateurs externes. </h3>
+      <h3>Cependant, il a également constaté que les patients avaient souvent des complications, telles
+      que des infections et des retards de guérison, dues à la compression des tissus mous par les fixateurs.
+    </h3>
+    <h3>
+      Pour résoudre ce problème, Ilizarov a conçu un appareil qui consistait en un cadre en métal autour du membre blessé,
+      avec des broches traversant la peau et les os.  Les broches étaient attachées au cadre avec des vis et des boulons,
+      et les patients étaient encouragés à se déplacer avec l'appareil. Les broches permettaient une fixation osseuse
+      stable sans comprimer les tissus mous, favorisant ainsi la guérison rapide.
+    </h3>
+    <h2>
+      Anecdotes sur l'appareil d'Ilizarov :
+    </h2>
+    <h3>
+      Forrest Gump dans le film éponyme porte durant son enfance cet appareil , qui est à l'origine de la scène mythique
+      "Cours Forrest"
+    </h3>
+    <h3>
+      En 1994, le joueur de hockey sur glace Vladimir Konstantinov a subi une blessure à la tête dans un accident de
+      voiture qui a mis sa vie en danger. Après avoir subi une intervention chirurgicale pour stabiliser sa colonne
+      vertébrale, il a été traité avec un appareil d'Ilizarov pour guérir les fractures de son fémur. Konstantinov a
+      finalement récupéré de sa blessure et a pu jouer à nouveau au hockey sur glace.
+    </h3>
+    <h3>
+      En 2006, le coureur d'ultramarathon américain Scott Jurek a subi une fracture de stress à la jambe pendant une
+      course de 160 km. Il a été traité avec un appareil d'Ilizarov et a été en mesure de reprendre la course plusieurs
+      mois plus tard.
+    </h3>
+    <h2>
+      L'appareil d'Ilizarov aujourd'hui :
+    </h2>
+    <h3>
+      Aujourd'hui, l'appareil d'Ilizarov est utilisé dans le monde entier pour traiter une variété de conditions
+      orthopédiques, y compris les fractures osseuses, les allongements osseux et les déformations. L'appareil peut
+      également être utilisé pour traiter les maladies infectieuses des os, en favorisant la cicatrisation des os et en
+      permettant aux antibiotiques d'atteindre la zone infectée.</h3>
+      <h3>
+      Le processus de traitement avec l'appareil d'Ilizarov peut prendre plusieurs mois, mais il est souvent efficace dans
+      des cas difficiles qui n'ont pas pu être traités avec d'autres méthodes. Les patients peuvent continuer à travailler
+      et à aller à l'école pendant le traitement, bien que cela puisse être difficile en raison de la gêne et de la
+      mobilité limitée.
+    </h3>
+  </v-container>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
+<style>#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -23,4 +74,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+
+h2 {
+  text-align: left;
+  margin: 2%;
+}
+
+h3 {
+  text-align: start;
+  margin-left: 3%;
+  margin-right: 3%;
+}</style>
